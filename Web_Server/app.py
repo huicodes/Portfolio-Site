f44fe533af3511801fe672b9f1569f4ccb1f43b0
huicodes <hikecodes@gmail.com>
2022-04-14 12:51:00 -0500

    Majority of these changes are from version upgrade

--- Web_Server/app.py
+++ Web_Server/app.py
@@ -1,3 +1,5 @@
+import mysql.connector
+from datetime import datetime
 from email import message
 from flask import Flask, render_template, request, url_for, redirect
 import csv
@@ -27,12 +29,32 @@ def write_to_csv(data):
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email,subject,message])
 
+def write_to_db(data):
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        passwd="p@ssw0rd1",
+        database="recruiter_db"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE TABLE contact_info (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, email VARCHAR(150) NOT NULL, company VARCHAR(50) NOT NULL, subject VARCHAR(255) NOT NULL, message NOT NULL, created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+    sql = "INSERT INTO contact_info (name, email, company, subject, message) VALUES (%s, %s, %s, %s, %s)"
+    val = (data["name"], data["email"], data["company"], data["subject"], data["message"], datetime.now())
+    mycursor.execute(sql, val)
+    mydb.commit()
+    mycursor.close()
+    mydb.close()
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_csv(data)
+        #write_to_csv(data)
+        write_to_db(data)
         print(data)
         return redirect('/thankyou.html')
     else:
         return 'somthing went wrong. Try again!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
