00653724401771f6d24a4deb93f6b0b4a62d8638
huicodes <hikecodes@gmail.com>
2022-04-16 13:45:58 -0500

    Altered table info

--- Web_Server/app.py
+++ Web_Server/app.py
@@ -37,22 +37,7 @@ def write_to_db(data):
     cursor.execute(sql, val)
     mydb.commit()
     print(cursor.rowcount, "record inserted.")
-    #mydb = mysql.connector.connect(
-    #    host="mysqldb",
-    #    user="root",
-    #    passwd="p@ssw0rd1",
-    #    database="recruiter_db"
-    #)
     
-    #mycursor = mydb.cursor()
-    #mycursor.execute("CREATE TABLE contact_info (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, email VARCHAR(150) NOT NULL, company VARCHAR(50) NOT NULL, subject VARCHAR(255) NOT NULL, message NOT NULL, created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
-    #sql = "INSERT INTO contact_info (name, email, company, subject, message) VALUES (%s, %s, %s, %s, %s)"
-    #val = (data["name"], data["email"], data["company"], data["subject"], data["message"], datetime.now())
-    #mycursor.execute(sql, val)
-    #mydb.commit()
-    #mycursor.close()
-    #mydb.close()
-
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
--- Web_Server/database.py
+++ Web_Server/database.py
@@ -10,17 +10,3 @@ config = {
 mydb = mysql.connector.connect(**config)
 cursor = mydb.cursor()
 
-TABLES = {}
-
-TABLES['contact_info'] = (
-    "CREATE TABLE IF NOT EXISTS `contact_info` ("
-    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `name` varchar(50) NOT NULL,"
-    "  `email` varchar(150) NOT NULL,"
-    "  `company` varchar(50) NOT NULL,"
-    "  `subject` varchar(255) NOT NULL,"
-    "  `message` text NOT NULL,"
-    "  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-    "  PRIMARY KEY (`id`)"
-    ") ENGINE=InnoDB"
-)
